da7959e920fabc0d7e50a9a2625748dc52167b38
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-07-21 05:32:08 +0900

    git username change test

--- delionserver/delionapi/views.py
+++ delionserver/delionapi/views.py
@@ -9,7 +9,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
